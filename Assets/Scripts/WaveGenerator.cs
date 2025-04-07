4b54ad476866e8f5e3c5e9437172f5e32b3a7c7e
James Richer <richerjames@gmail.com>
2025-04-07 23:06:18 +0100

    removed debug

--- Assets/Scripts/WaveGenerator.cs
+++ Assets/Scripts/WaveGenerator.cs
@@ -3,9 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-// This is a test comment
-// This is a line for blog-branch
-
 namespace OceanRendering
 {
     [RequireComponent(typeof(MeshRenderer))]
